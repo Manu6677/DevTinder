96cfa4d3964570ad0b82ca01a776f5ed320bdf7b
manu <manu.panjoria666@gmail.com>
2024-10-16 01:12:40 +0530

    Season-02/till/Ep-04

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 
 const app = express();
 
-// Router handler
-app.use("/hello", (req, res)=>{
-    console.log("Hello from Server");
-    res.send("Hello from server")
+// Router handlers
+
+//Below is the Query route example
+app.get("/user/", (req, res)=>{
+    res.send("hello devs");
+    console.log(req.query);
+})
+
+
+//Below is Dynamic route example
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
+    res.send("Dynamic routes")
 })
 
 app.listen(8000,()=>{
